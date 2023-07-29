49b41a7473ab0babd301829992af2977a6acfe92
joao_scorissa <joao_scorissa@hotmail.com>
2023-07-29 16:12:02 -0300

    fix

--- main.go
+++ main.go
@@ -2,15 +2,10 @@ package main
 
 import (
 	"github.com/joaoscorissa/go-api-rest/database"
-	"github.com/joaoscorissa/go-api-rest/models"
 	"github.com/joaoscorissa/go-api-rest/routes"
 )
 
 func main() {
-	models.Personalidades = []models.Personalidade{
-		{Id: 1, Nome: "João", Historia: "João..."},
-		{Id: 2, Nome: "Maria", Historia: "Maria..."},
-	}
 	database.ConnectDB()
 	routes.HandleRequest()
 }
